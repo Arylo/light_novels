33f33cacbf0cbc632454d8e721cf82620f7d0d7b
AryloYeung <arylo.open@gmail.com>
2019-04-08 10:40:25 +0800

    chore: update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-setTimeout(function () {
-    if (/(\/|\/index.html)$/.test(location.pathname)) {
+setTimeout(function () {    
+    if ("/" === location.pathname || "/index.html" === location.pathname) {
+        document.querySelector('a.navigation-next').remove();
+    } else if (/(\/|\/index.html)$/.test(location.pathname)) {
         document.querySelector('a.navigation-prev').remove();
         document.querySelector('a.navigation-next').className = document.querySelector('a.navigation-next').className + " navigation-unique";
     }
