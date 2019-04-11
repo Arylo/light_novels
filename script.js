38a57aeeba5e9f21f652c7572945273986163f77
AryloYeung <arylo.open@gmail.com>
2019-04-12 01:14:42 +0800

    chore: update script

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ require(['gitbook', 'jquery'], function(gitbook, $) {
     gitbook.events.on('page.change', function() {
         if ("/" === location.pathname || "/index.html" === location.pathname) {
             $('a.navigation-next').remove();
+            $('link[rel=prev]').remove();
         } else if (/(\/|\/index.html)$/.test(location.pathname)) {
             $('a.navigation-prev').remove();
+            $('link[rel=prev]').remove();
             if (!$('a.navigation-next').hasClass("navigation-unique")) {
                 $('a.navigation-next').addClass("navigation-unique");
             }
